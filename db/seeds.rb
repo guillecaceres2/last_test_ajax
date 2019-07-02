4dc39e5e82cb5285eaeb21086ab835e41af79e0d
gmocaceres <guillecaceres1989@gmail.com>
2019-07-02 13:37:36 -0400

    usado el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'putin@blyatman.urss', password: 'stalin2.0', password_confirmation: 'stalin2.0') if Rails.env.development?
+
+Company.destroy_all
+User.destroy_all
+Claim.destroy_all
+
+20.times do
+Company.create(
+name: Faker::Company.name
+)
+end
+
+30.times do
+User.create(
+email: Faker::Internet.email,
+password: 123456
+)
+end
+
+users = User.all
+companies = Company.all
+
+300.times do
+Claim.create(
+content: Faker::Lorem.sentence,
+user: users.sample,
+company: companies.sample
+)
+end
