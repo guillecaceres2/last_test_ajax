ae3de22197445cc4fe233a43ab09d95281c9b73b
gmocaceres <guillecaceres1989@gmail.com>
2019-07-02 13:59:09 -0400

    respaldo de prueba

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,9 +3,10 @@ class CompaniesController < ApplicationController
 
   # GET /companies
   # GET /companies.json
-  def index
-    @companies = Company.all
-  end
+def index
+@companies = Company.all
+@company = Company.new
+end
 
   # GET /companies/1
   # GET /companies/1.json
@@ -23,19 +24,12 @@ class CompaniesController < ApplicationController
 
   # POST /companies
   # POST /companies.json
-  def create
-    @company = Company.new(company_params)
-
-    respond_to do |format|
-      if @company.save
-        format.html { redirect_to @company, notice: 'Company was successfully created.' }
-        format.json { render :show, status: :created, location: @company }
-      else
-        format.html { render :new }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+def create
+@company = Company.create(company_params)
+respond_to do |format|
+format.js
+end
+end
 
   # PATCH/PUT /companies/1
   # PATCH/PUT /companies/1.json
@@ -53,13 +47,12 @@ class CompaniesController < ApplicationController
 
   # DELETE /companies/1
   # DELETE /companies/1.json
-  def destroy
-    @company.destroy
-    respond_to do |format|
-      format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+def destroy
+@company.destroy
+respond_to do |format|
+format.js
+end
+end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -71,4 +64,5 @@ class CompaniesController < ApplicationController
     def company_params
       params.require(:company).permit(:name)
     end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'claims/create'
   resources :companies
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
