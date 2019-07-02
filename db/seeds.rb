56720bfc623ac8071cb7a586dbc7cf481e8b2d47
gmocaceres <guillecaceres1989@gmail.com>
2019-07-02 13:16:37 -0400

    creado scaffold company y migraciones casi listas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'putin@blyatman.urss', password: 'stalin2.0', password_confirmation: 'stalin2.0') if Rails.env.development?
